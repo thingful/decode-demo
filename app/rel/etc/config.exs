0ddc194ec4bf17c90fef9083115a4a5de2b9b175
Sam Mulube <sam@umbrellium.co.uk>
2019-01-06 12:46:44 +0000

    read production tls port from environment

--- app/rel/etc/config.exs
+++ app/rel/etc/config.exs
@@ -5,7 +5,7 @@ config :demo, DecodeDemoWeb.Endpoint,
   url: [host: System.get_env("HOST"), port: {:system, "PORT"}],
   https: [
     otp_app: :demo,
-    port: 4001,
+    port: System.get_env("TLS_PORT"),
     cipher_suite: :strong,
     keyfile: "priv/cert/decodedemo.dev+3-key.pem",
     certfile: "priv/cert/decodedemo.dev+3.pem"
