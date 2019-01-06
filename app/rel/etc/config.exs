3e175beae1d73c51386b7be1af90575ffda4145b
Sam Mulube <sam@umbrellium.co.uk>
2019-01-06 11:53:02 +0000

    Add prod config using our self signed certs for now
    This is just to run in the system test under ssl, if we deploy to
    Heroku, we will terminate TLS before we reach our app so won't need this
    configuration.

--- app/rel/etc/config.exs
+++ app/rel/etc/config.exs
@@ -2,4 +2,11 @@ use Mix.Config
 
 config :demo, DecodeDemoWeb.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  url: [host: System.get_env("HOST"), port: {:system, "PORT"}]
+  url: [host: System.get_env("HOST"), port: {:system, "PORT"}],
+  https: [
+    otp_app: :demo,
+    port: 4001,
+    cipher_suite: :strong,
+    keyfile: "priv/cert/decodedemo.dev+3-key.pem",
+    certfile: "priv/cert/decodedemo.dev+3.pem"
+  ]
